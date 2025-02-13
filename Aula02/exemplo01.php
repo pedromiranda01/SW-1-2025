34c75c2c68a39ffee2a79424192275fcd7cdfe5a
Goht <161866585+pedromiranda01@users.noreply.github.com>
2025-02-13 18:22:41 -0300

    Add files via upload

---
+++ Aula02/exemplo01.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Oi 2F, sou o Pedro</p>
+    <?php
+       $nome = "abelha";
+       echo "<p>Oi 2F, sou o Pedro</p>";
+       echo "<p>Oi 2F, sou o $nome</p>"
+       echo "<p>Oi 2F, sou o " .$nome."</p>";
+
+     ?>
+</body>
+</html>
