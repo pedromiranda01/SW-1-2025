8c95266dac0b4bff7256e5323d4abf8829a6f3af
Goht <161866585+pedromiranda01@users.noreply.github.com>
2025-02-17 15:00:25 -0300

    Add files via upload

---
+++ Aula02/exemplo06.php
@@ -0,0 +1,18 @@
+<?php
+  $nome = "Anderson";
+  $idade = 90;
+  $funcao = "professor";
+
+  $frase = "Dados Pessoas:";
+  $frase .= $nome;
+  $frase .= " - ";
+  $frase .= $idade;
+  $frase .= " - ";
+  $frase .= $funcao;
+
+  echo $frase;
+
+
+
+
+?>
